326ba6022c17e7f710ebd97bef44dcb0b24ba474
Tairik Johnny <tairikjohnny@gmail.com>
2022-05-24 16:25:54 -0300

    Documentando a classe de teste de integração

--- src/test/java/api/theVelopers/sas/service/impl/IntegracaoTest.java
+++ src/test/java/api/theVelopers/sas/service/impl/IntegracaoTest.java
@@ -15,16 +15,25 @@ public class IntegracaoTest
   @Test
   public void testeIntegracao() {
 
+    // Setando o WebDriverManager que substitui o chromedriver
     WebDriverManager.chromedriver().setup();
+
+    // Criando um objeto pra setar as configurações do navegador
     ChromeOptions options = new ChromeOptions();
-//    options.addArguments("--no-sandbox");
-//    options.addArguments("--disable-dev-shm-usage");
-//    options.addArguments("--headless");
+
+    // Setando as configurações do navegador pra ele não iniciar uma renderização visual
+    // e desabilitar possiveis elementos que podem gerar erro no teste
+    options.addArguments("--no-sandbox");
+    options.addArguments("--disable-dev-shm-usage");
+    options.addArguments("--headless");
+
+    // Criando um objeto e setando as configurações do navegador
     driver = new ChromeDriver(options);
 
-    driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-    driver.manage().window().maximize();
+    // Abrindo o navegador com a url informada
     driver.get(baseUrl);
+
+    // Fechando o navegador
     driver.close();
   }
 }
