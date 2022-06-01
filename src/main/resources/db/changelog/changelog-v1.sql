299d7e3c4d93b142281159e6ad40004fbd81de36
jef771 <jefh.neves@gmail.com>
2022-05-31 22:08:25 -0300

    feature/A5S-172 - Criado migration com flyway

--- src/main/resources/db/changelog/changelog-v1.sql
+++
@@ -1,2 +0,0 @@
--- changeset Jeferson:1:insert
-insert into consumo(emp_cnpj, cons_mesref, cons_consumo) values(203611000137,'31/07/2022 00:00:00',1331)
---
+++ src/main/resources/db/migration/V1_database.sql
@@ -0,0 +1,1 @@
+insert into consumo(emp_cnpj, cons_mesref, cons_consumo) values(203611000137,'31/08/2022 00:00:00',1331)
